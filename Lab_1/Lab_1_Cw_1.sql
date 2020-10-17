dd207702a623aa72d4205313b4df9c9a6c29ad28
dimamik <dymitrmikielewicz@gmail.com>
2020-10-17 17:16:40 +0200

    Added Solutions to Lab_1

---
+++ Lab_1/Lab_1_Cw_1.sql
@@ -0,0 +1,65 @@
+﻿/* Setting default database */
+use library
+/* Lab 1 Cw_Koncowe 17.10.2020*/
+
+/*Zad 1 Przyjmujac ze title_no - numer ksiazki
+Napisz polecenie select, za pomocą którego uzyskasz tytuł 
+i numer książki*/
+
+-- select title,title_no from title
+
+
+/*Zad 2 Napisz polecenie, które wybiera tytuł o numerze 10*/
+
+-- select title
+-- from title
+-- where title_no=10
+
+
+/*Zad 3 Napisz polecenie, które wybiera numer czytelnika i karę 
+dla tych czytelników, którzy mają kary między $8 a $9*/
+
+-- select member_no, fine_assessed
+-- from loanhist
+-- where fine_assessed>=8 and fine_assessed<=9
+
+/* Zad 3 Sposob 2 */
+
+-- select member_no, fine_assessed
+-- from loanhist
+-- where fine_assessed between 8 and 9
+
+
+/*Zad 4 Napisz polecenie select, za pomocą którego uzyskasz 
+numer książki i autora dla wszystkich książek, których 
+autorem jest Charles Dickens lub Jane Austen*/
+
+-- select title_no, author
+-- from title
+-- where author='Charles Dickens' or author='Jane Austen'
+
+
+/*Zad 5 Napisz polecenie, które wybiera numer tytułu i tytuł dla 
+wszystkich rekordów zawierających string „adventures” 
+gdzieś w tytule.*/
+
+-- select title_no, title
+-- from title
+-- where title like '%adventures%'
+
+
+/*Zad 6  Napisz polecenie, które wybiera numer czytelnika, karę 
+oraz zapłaconą karę dla wszystkich, którzy jeszcze nie zapłacili.
+UWAGA! Zakladam ze dane nie zostaja odswiezone
+*/
+
+-- select member_no, fine_assessed, fine_paid
+-- from loanhist
+-- where ISNULL(fine_assessed,0) > ISNULL(fine_paid,0) + ISNULL(fine_waived,0)
+
+
+/*Zad 7 Napisz polecenie, które wybiera wszystkie unikalne pary 
+miast i stanów z tablicy adult.*/
+
+-- select DISTINCT city, state
+-- from adult
---
+++ Lab_1/Lab_1_Cw_2.sql
@@ -0,0 +1,81 @@
+/* Setting default database */
+use library
+
+/* Zad 1  Napisz polecenie, które wybiera wszystkie tytuły z tablicy 
+title i wyświetla je w porządku alfabetycznym.*/
+
+-- SELECT title
+-- from title
+-- ORDER BY title
+
+
+/* Zad 2  Napisz polecenie, które:
+     wybiera numer członka biblioteki, isbn książki i wartość 
+    naliczonej kary  dla wszystkich wypożyczeń, dla których 
+    naliczonej kary  dla wszystkich wypożyczeń, dla których 
+    naliczono karę
+     stwórz kolumnę wyliczeniową zawierającą podwojoną wartość 
+    kolumny fine_assessed
+     stwórz alias ‘double fine’ dla tej kolumny*/
+
+-- SELECT member_no,isbn,fine_assessed, fine_assessed*2 as 'double fine'
+-- from loanhist
+-- where ISNULL(fine_assessed,0)>0
+
+
+/* Zad 3 do modyfikacji
+Napisz polecenie, które
+     generuje pojedynczą kolumnę, która zawiera kolumny: imię 
+    członka biblioteki, inicjał drugiego imienia i nazwisko dla 
+    wszystkich członków biblioteki, którzy nazywają się Anderson
+     nazwij tak powstałą kolumnę „email_name” 
+     zmodyfikuj polecenie, tak by zwróciło „listę proponowanych 
+    loginów e-mail”  utworzonych przez połączenie imienia członka 
+    zmodyfikuj polecenie, tak by zwróciło „listę proponowanych 
+    loginów e-mail”  utworzonych przez połączenie imienia członka 
+    biblioteki, z inicjałem drugiego imienia i pierwszymi dwoma 
+    literami nazwiska (wszystko małymi literami). 
+     wykorzystaj funkcję SUBSTRING do uzyskania części kolumny 
+    znakowej oraz LOWER do zwrócenia wyniku małymi literami
+     wykorzystaj operator (+) do połączenia stringów.
+
+Do modyfikacji:
+*/
+
+select firstname + SPACE(1) + SPACE(1) + middleinitial + SPACE(1) + lastname as 'email_name'
+from member
+where lastname='Anderson'
+
+
+/* Zad 3 po modyfikacji */
+
+SELECT LOWER(
+    firstname + middleinitial + SUBSTRING(lastname,1,2)
+)
+from member
+
+
+/* Zad 4  Napisz polecenie, które wybiera title i title_no z tablicy 
+title.
+     Wynikiem powinna być pojedyncza kolumna o formacie jak w 
+    przykładzie poniżej: 
+        The title is: Poems, title number 7
+     Czyli zapytanie powinno zwracać pojedynczą kolumnę w oparciu 
+    o wyrażenie, które łączy 4 elementy:
+        stała znakowa ‘The title is:’
+        wartość kolumny title
+        stała znakowa ‘title number’
+        wartość kolumny title_no*/
+
+
+/* UWAGA! 
+    1sp -> Uzycie STR() 
+    2sp -> Uzycie CAST()*/
+
+-- select 'The title is: ' + title + ', title number' + STR(title_no)
+-- from title
+
+
+-- select 'The title is: ' + title + ', title number ' + CAST(title_no as varchar)
+-- from title
+
