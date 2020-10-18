2742d9743e1215c0e109b096c475b88dd91d718b
dimamik <dymitrmikielewicz@gmail.com>
2020-10-18 23:13:47 +0200

    Added Lab_1_Cw_0.sql

---
+++ Lab_1/Lab_1_Cw_0.sql
@@ -0,0 +1,120 @@
+/* Setting default database */
+use northwind
+
+/* Str 29/57  */
+
+/* Zad 1 */
+
+-- select ContactName, Address
+-- from Customers
+
+/* Zad 2 */
+
+-- select LastName, HomePhone
+-- from Employees
+
+/* Zad 3 */
+
+-- SELECT ProductName, UnitPrice
+-- from Products
+
+/* Zad 4 */
+
+-- select CategoryName,Description
+-- from Categories
+
+/* Zad 5 */
+
+-- select CompanyName, HomePage
+-- from Suppliers
+
+/*  Str 34/57 */
+
+/* Zad 1 */
+
+-- select CompanyName, Address
+-- from Customers
+-- where City='London'
+
+/* Zad 2 */
+
+-- select CompanyName, Address
+-- from Customers
+-- where Country = 'France' or Country='Spain'
+
+/* Zad 3 */
+
+-- select ProductName,UnitPrice
+-- from Products
+-- where UnitPrice BETWEEN 20 and 30
+
+/* Zad 4 */
+
+-- select ProductName,UnitPrice
+-- from Products
+-- where CategoryID in (
+--     select CategoryID
+--     from Categories
+--     where CategoryName LIKE '%Meat%'
+-- )
+
+/* Zad 5  V1*/
+
+-- select ProductName,UnitsInStock
+-- from Products
+-- where SupplierID in (
+--     select SupplierID
+--     from Suppliers
+--     where CompanyName='Tokyo Traders'
+-- )
+
+/* Zad 5 V2 */
+
+-- select ProductName,UnitsInStock
+-- from Products
+-- where SupplierID = 4
+
+/* Zad 6  */
+
+-- select ProductName 
+-- from Products
+-- where Discontinued=1 or UnitsInStock<=0
+
+/* Str 38/57 */
+
+/* Zad 1 */
+
+-- select * 
+-- from Products
+-- where QuantityPerUnit like '%bottle%'
+
+
+/* Zad 2 */
+
+-- select *
+-- from Employees
+-- where LastName like '[B-L]%'
+-- ORDER BY LastName
+
+/* Zad 3 */
+
+-- select *
+-- from Employees
+-- where LastName like '[BL]%'
+-- ORDER BY LastName
+
+/* Zad 4 */
+
+-- SELECT CategoryName
+-- from Categories
+-- where [Description] LIKE '%,%'
+
+/* Zad 5 */
+
+-- select *
+-- from Customers
+-- where CompanyName LIKE '%store%'
+
+/* Dalej tez sa zadania ... */
+
+
